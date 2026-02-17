0bfaa9da71f0ac8ebc3512df7da589111bd5db58
Erwann Gache <egache@z4r6p1.42lyon.fr>
2026-02-17 18:06:33 +0100

    need to remove voidBank from Character class and do it in main

--- ex03/src/main.cpp
+++ ex03/src/main.cpp
@@ -6,6 +6,46 @@
 #include "Ice.hpp"
 #include "MateriaSource.hpp"
 
+// int main()
+// {
+//   MateriaSource *src = new MateriaSource();
+//   AMateria *spell = new Ice();
+//   AMateria *spell2 = new Ice();
+//   AMateria *spell3;
+//
+//   *spell2 = *spell;
+//   spell3 = spell2;
+//
+//   IMateriaSource *src2 = new MateriaSource(*src);
+//   src2->learnMateria(spell);
+//   src2->learnMateria(spell2);
+//   src2->learnMateria(spell3);
+//   src->learnMateria(new Cure());
+//   ICharacter *me = new Character("me");
+//   AMateria *tmp;
+//   //   tmp = src->createMateria("eci");
+//   //   me->equip(tmp);
+//   //   tmp = src->createMateria("ice");
+//   //   me->equip(tmp);
+//   tmp = src->createMateria("cure");
+//   me->equip(tmp);
+//   tmp = src->createMateria("cure");
+//   me->equip(tmp);
+//   tmp = src2->createMateria("ice");
+//   me->equip(tmp);
+//   ICharacter *bob = new Character("bob");
+//   me->use(0, *me);
+//   me->use(1, *bob);
+//   me->unequip(1);
+//   me->equip(tmp);
+//   me->use(2, *me);
+//   Character::deleteVoidBank();
+//   delete bob;
+//   delete me;
+//   delete src;
+//   delete src2;
+//   return 0;
+// }
 int main()
 {
   MateriaSource *src = new MateriaSource();
@@ -40,7 +80,7 @@ int main()
   me->use(3, *me);
   me->unequip(3);
   me->equip(tmp);
-  me->use(3, *me);
+  me->use(1, *me);
   me->unequip(3);
   me->equip(tmp);
   me->use(3, *me);
